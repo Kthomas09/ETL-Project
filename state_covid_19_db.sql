b4cbb34a1f17a71b5b4b0ebcddc8efe6116a601b
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-09-26 21:33:45 -0400

    completed the readme and cleaned the code.

---
+++ state_covid_19_db.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS state_covid_19 ;
+
+CREATE TABLE state_covid_19 (
+  todays_date DATE,
+  positive FLOAT,
+  negative FLOAT,
+  death_increase VARCHAR(30),
+  state  VARCHAR(30),
+  total_cases INT,
+  death INT,
+	updated VARCHAR(30),
+	state_abrv VARCHAR(30)
+); 
+
+COPY state_covid_19(todays_date, positive, negative, death_increase, state, total_cases, death, updated, state_abrv)
+FROM '/Users/kent.thomas/Repository/ETL-Project/Resources/combinedCovidData.csv'
+DELIMITER ','
+CSV HEADER;
+	
+SELECT *
+FROM state_covid_19;
